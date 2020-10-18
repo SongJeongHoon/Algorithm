f36801474da898e7e2b711bed2a535abb8c08661
SongJeongHoon <43517129+SongJeongHoon@users.noreply.github.com>
2020-10-18 17:07:25 +0900

    Tree
    Start!

---
+++ Tree/Tree.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int Data;
+	Node* LeftChiled;
+	Node* RightChiled;
+};
+
+void Push(int _data, );
+
+int main()
+{
+	Node* RootNode = nullptr;
+}
